01898b1f0a1064ee22651a4142bd02770a4644fc
Francesco fantera <francescofantera08@gmail.com>
2024-10-15 09:34:42 +0200

    Project setup + first program

---
+++ Project_git_first/Project_git_first/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("right-angled triangle hypotenuse");
+
+Console.WriteLine("insert the lenght side");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("insert the height side");
+double b = double.Parse(Console.ReadLine());
+
+double i = Math.Sqrt(a * a + b * b);
+
+Console.Write("lenght side: ");
+Console.WriteLine(a);
+Console.Write("height side: ");
+Console.WriteLine(b);
+
+Console.WriteLine(i);
